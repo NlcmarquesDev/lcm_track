6d0652702de9463bf227da90523887ca36f71676
Jose <jose@eurocomsoftware.com>
2025-12-10 14:47:55 +0100

    feito o design da app

--- frontend/src/app/components/modal-add-record/modal-add-record.component.ts
+++ frontend/src/app/components/modal-add-record/modal-add-record.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal-add-record',
   imports: [],
   templateUrl: './modal-add-record.component.html',
-  styleUrl: './modal-add-record.component.scss'
+  styleUrl: './modal-add-record.component.scss',
 })
 export class ModalAddRecordComponent {
+  @Output() cancel = new EventEmitter<void>();
 
+  onCancel() {
+    this.cancel.emit(); // <- avisa o pai
+  }
 }
--- frontend/src/app/components/modal-settings/modal-settings.component.ts
+++ frontend/src/app/components/modal-settings/modal-settings.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal-settings',
   imports: [],
   templateUrl: './modal-settings.component.html',
-  styleUrl: './modal-settings.component.scss'
+  styleUrl: './modal-settings.component.scss',
 })
 export class ModalSettingsComponent {
+  @Output() cancel = new EventEmitter<void>();
 
+  onCancel() {
+    this.cancel.emit(); // <- avisa o pai
+  }
 }
