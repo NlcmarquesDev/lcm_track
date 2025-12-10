6a661c74c92e908bb1c1a510d62507d593b8b75d
Jose <jose@eurocomsoftware.com>
2025-12-10 13:59:31 +0100

    done the design from close month modal, and btn  to return back

--- frontend/src/app/components/modal-close-month/modal-close-month.component.ts
+++ frontend/src/app/components/modal-close-month/modal-close-month.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal-close-month',
   imports: [],
   templateUrl: './modal-close-month.component.html',
-  styleUrl: './modal-close-month.component.scss'
+  styleUrl: './modal-close-month.component.scss',
 })
 export class ModalCloseMonthComponent {
+  @Output() cancel = new EventEmitter<void>();
 
+  onCancel() {
+    this.cancel.emit(); // <- avisa o pai
+  }
 }
